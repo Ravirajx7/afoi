604bfa79721df2311349bdb45a76d4230511f90f
Shashank J <www.shashankreddy@gmail.com>
2018-01-25 04:41:09 -0800

    Rendering About, News and Contact

---
+++ src/components/About.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import './Aboutcss.css';
+
+class About extends Component {
+	render () {
+		return (
+			<div className = "header">
+				About us.
+			</div>
+		);
+	}
+}
+
+export default About;
